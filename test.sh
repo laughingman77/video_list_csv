21bbdb6fd8a1bb8459d020c65aa4bf230470567f
lauhging man <8545282-laughing_man77@users.noreply.gitlab.com>
2024-03-14 08:27:44 +1100

    Tidy test.sh.

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Find all script files and shellcheck them
+# Manual testing: Find all script files and shellcheck them
 # @see https://github.com/koalaman/shellcheck/issues/143
-for f in $({ find . -type f -regex ".*\.\w*sh"
-    file ./* | grep '#!\(/usr/bin/env \|/bin/\)sh' | cut -d: -f1
+for f in $({ find . -type f -regex '.*\.\w*sh'
+    file ./* | grep 'shell script' | cut -d: -f1
     } | sort -u); do
   shellcheck "$f"
 done
